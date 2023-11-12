6f084c6ffa433ba5be78051a6b07e66d5894988d
ecortesg <cgarcia.ernesto@gmail.com>
2023-11-11 23:46:17 -0600

    Removed is_deleted field

--- core/models.py
+++ core/models.py
@@ -12,7 +12,6 @@ class AbstractBaseModel(models.Model):
     )
     created_at = models.DateTimeField("created at", auto_now_add=True)
     updated_at = models.DateTimeField("updated at", auto_now=True)
-    is_deleted = models.BooleanField(default=False)
 
     class Meta:
         abstract = True
